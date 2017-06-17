c51b12adfac476a9e1d5e07e3dc1ac4bd7b6bbfa
admintuan <admintuan@gmail.com>
2017-06-17 15:27:14 +0700

    Complete!

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
 import React, {Component} from 'react';
-import {View, Text } from 'react-native';
+import {View, Text, FlatList, Image, StyleSheet} from 'react-native';
 
 export default class App extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            mang:[]
+        }
+    }
     render(){
         return(
-            <View style={{backgroundColor:'#fff', justifyContent:'center', alignItems:'center', padding:50}}
-            >
-                <Text> Hello </Text>
-            </View>
+            <FlatList
+                data={this.state.mang}
+                renderItem={
+                    ({item})=><View style={ao.dong}>
+                        <View style={ao.trai}>                        
+                            <Image style={ao.hinh}
+                            source={{uri: item.Hinh}}
+                            />
+                        </View>
+                        <View style={ao.phai}>
+                            <Text>{item.Name}</Text>
+                            <Text>{item.key}</Text>
+                        </View>
+                    </View>
+                }
+               
+            />
         );
     }
+    componentDidMount() {
+       fetch('http://192.168.1.5/flatlist/Casi.json')
+        .then((response)=>response.json())
+        .then((responseJson)=>{
+            this.setState({
+                mang: responseJson
+            });
+        })
+        .catch((error)=>{
+        console.log(error);
+        });
+    }
 }
+var ao= StyleSheet.create({
+    dong: {padding: 10, borderBottomWidth:1, borderRightWidth:1,  
+        justifyContent:'center', alignItems:'center', flexDirection:'row'},
+    trai:{flex:1, justifyContent:'center', alignItems:'center'},
+    phai:{flex:1, justifyContent:'center', alignItems:'center'},
+    hinh:{height: 100, width: 100, borderRadius:100}
+});
